982078236330b5f984f1ec96ce3a5df0b1ae5177
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-12-16 22:49:21 +0600

    first commit

---
+++ components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Header() {
+    return (
+        <View>
+            <Text>this is header</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
---
+++ components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Home() {
+    return (
+        <View>
+            <Text>this is home page</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
