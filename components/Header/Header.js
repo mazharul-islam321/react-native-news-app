ac58d221881eb0bdf3f29c2c06918a5551273975
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-12-18 04:50:20 +0600

    display news design

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,19 +4,25 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 export default function Header() {
     return (
         <View style={styles.navbarStyle}>
-            <Text style={styles.icon}>NEWS</Text>
-            <TextInput
-                placeholder="search your country"
-                style={styles.headerSearch}
-            ></TextInput>
-            <Text>Sign In</Text>
-            {/* <Text>SignOut</Text> */}
+            <View style={styles.navbarStylein}>
+                <Text style={styles.icon}>NEWS</Text>
+                <TextInput
+                    placeholder="search your country"
+                    style={styles.headerSearch}
+                ></TextInput>
+                <Text>Sign In</Text>
+                {/* <Text>SignOut</Text> */}
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     navbarStyle: {
+        paddingBottom: 10,
+        backgroundColor: "rgba(0, 0, 0, 0.2)",
+    },
+    navbarStylein: {
         marginTop: 30,
         marginLeft: 10,
         marginRight: 10,
@@ -32,5 +38,6 @@ const styles = StyleSheet.create({
         borderRadius: 12,
         paddingLeft: 20,
         paddingRight: 20,
+        backgroundColor: "rgba(235, 235, 235, .7)",
     },
 });
--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,13 +1,38 @@
-import React from "react";
-import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 
 export default function Home() {
+    const [news, setNews] = useState([]);
+    useEffect(() => {
+        const uri = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=58c978c81f1a4eed8e09fca220bb594c`;
+        fetch(uri)
+            .then((res) => res.json())
+            .then((data) => setNews(data.articles));
+    }, []);
+    // console.log(news);
     return (
-        <ScrollView>
-            {/* <FlatList></FlatList> */}
-            <Text>this is home page</Text>
-        </ScrollView>
+        <View>
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                data={news}
+                renderItem={({ item }) => (
+                    <View key={item?.source?.id} style={styles.section}>
+                        <Text>{item?.title}</Text>
+                        <Image
+                            source={{ uri: `${item?.urlToImage}` }}
+                            style={{ width: "100%", height: 200 }}
+                        />
+                        <Text>{item?.author}</Text>
+                    </View>
+                )}
+            ></FlatList>
+        </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    section: {
+        borderBottomWidth: 10,
+        borderBottomColor: "gray",
+    },
+});
